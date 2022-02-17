b4fa5cb5e08ab47a9a6107962a883084e19a64b7
Ficko Azhar <ficko2167@gmail.com>
2022-02-17 11:47:39 +0800

    menambahkan folder tugas_bootstrap, tugas_css, tugas_html, tugas_javascript, tugas_jquery dan tugas_php

---
+++ tugas_php/bangun_datar.php
@@ -0,0 +1,31 @@
+<?php
+// MENGHIUNG LUAS BANGUN DATAR
+// LUAS PERSEGI PANJANG
+$panjang = 10;
+$lebar = 17;
+$luas_persegi_pjg = $panjang * $lebar;
+echo" luas persegi dengan panjang : $panjang cm dan lebar : $lebar cm adalah $luas_persegi_pjg";
+
+// LUAS PERSEGI
+$sisi = 10;
+$luas_persegi = $sisi * $sisi;
+echo" luas persegi dengan panjang sisi : $sisi cm adalah $luas_persegi";
+
+// LUAS SEGITIGA
+$a = 10;
+$t = 17;
+$luas_segitiga = 0.5 * $a * $t;
+echo" luas persegi dengan alas : $a cm dan tinggi : $t cm adalah $luas_segitiga";
+
+// LUAS JAJAR GENJANG
+$a = 10;
+$t = 17;
+$luas_jgenjang = $a * $t;
+echo" luas persegi dengan alas : $a cm dan tinggi : $t cm adalah $luas_jgenjang";
+
+// LUAS BELAH KETUPAT
+$d1 = 10;
+$d2 = 17;
+$luas_bketupat = 0.5 * $d1 * $d2;
+echo" luas persegi dengan panjang diagonal1 : $d1 cm dan diagonal2 : $d2 cm adalah $luas_bketupat";
+?>
